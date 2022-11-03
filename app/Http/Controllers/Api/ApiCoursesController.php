6e31f681a9cdeaaf740a147cb9689260e235ec0e
Jorge2824 <martinezpisfil2004@gmail.com>
2022-11-03 02:00:45 -0500

    Changes for Names

--- app/Http/Controllers/Api/ApiCoursesController.php
+++ app/Http/Controllers/Api/ApiCoursesController.php
@@ -10,7 +10,7 @@ class ApiCoursesController extends Controller
 {
     public function searchCourses(Request $request) {
         $parent_id = $request["parent_id"];
-        $statement = "select c.*, m.id parent_id from marks_by_course m join courses c on c.id = m.course_id
+        $statement = "select c.*, m.id parent_id, m.name_mark name from marks_by_course m join courses c on c.id = m.course_id
         where if(? = 0, m.parent_id is null, m.parent_id = ?)";
         $params = [$parent_id, $parent_id];
         $data = DB::select($statement, $params);
